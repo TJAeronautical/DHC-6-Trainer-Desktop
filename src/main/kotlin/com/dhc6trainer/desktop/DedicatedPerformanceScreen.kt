3cf63ace11b454c906d6f9e3a2865e0a06172d1c
TJ Aeronautical <tjaeronautical@gmail.com>
2026-07-19 15:46:43 +0500

    Add redesigned Step 7 performance screen

---
+++ src/main/kotlin/com/dhc6trainer/desktop/DedicatedPerformanceScreen.kt
@@ -0,0 +1,9 @@
+package com.dhc6trainer.desktop
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
