2f0cf3965b2d644e815d1cf0a3f1cebe27945da8
TJ Aeronautical <tjaeronautical@gmail.com>
2026-07-19 14:53:37 +0500

    Add dedicated checklist screen

---
+++ src/main/kotlin/com/dhc6trainer/desktop/DedicatedChecklistScreen.kt
@@ -0,0 +1,10 @@
+package com.dhc6trainer.desktop
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose
